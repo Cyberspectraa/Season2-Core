2087f0e4cf1bcda7e5d8a3f7158e8856e35e8f09
Cyberspectraa <CJonRoberts2005@gmail.com>
2026-09-15 11:39:54 +0100

    test: bootstrap registries for Path Wand settings

--- src/test/java/com/season2/townlife/item/PathWandSettingsTest.java
+++ src/test/java/com/season2/townlife/item/PathWandSettingsTest.java
@@ -3,14 +3,23 @@ package com.season2.townlife.item;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.season2.townlife.data.TownPathType;
-import net.minecraft.world.item.Item;
+import net.minecraft.SharedConstants;
+import net.minecraft.server.Bootstrap;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 final class PathWandSettingsTest {
+    @BeforeAll
+    static void bootstrapMinecraftRegistries() {
+        SharedConstants.tryDetectVersion();
+        Bootstrap.bootStrap();
+    }
+
     @Test
     void settingsRoundTripOnTheWandStack() {
-        ItemStack stack = new ItemStack(new Item(new Item.Properties()));
+        ItemStack stack = new ItemStack(Items.STICK);
 
         PathWandSettings.set(stack, PathEditMode.REMOVE_CONNECTED, TownPathType.AVOID);
 
@@ -20,7 +29,7 @@ final class PathWandSettingsTest {
 
     @Test
     void freshWandDefaultsToConnectedNormalPath() {
-        ItemStack stack = new ItemStack(new Item(new Item.Properties()));
+        ItemStack stack = new ItemStack(Items.STICK);
 
         assertEquals(PathEditMode.ADD_CONNECTED, PathWandSettings.mode(stack));
         assertEquals(TownPathType.NORMAL, PathWandSettings.type(stack));
